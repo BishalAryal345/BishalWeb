bd4470de0c110d8ec0322785496cc06922cc8856
BishalAryal345 <sirajaryal69@gmail.com>
2021-02-25 10:43:51 +0545

    Login success

--- src/Container/Login.js
+++ src/Container/Login.js
@@ -1,13 +1,32 @@
 import { Component } from "react";
-
+import axios from 'axios';
 class Login extends Component{
+    state={
+        Username : "",
+        Password : ""
+    }
+    sendUserData=()=>{
+        const data = {
+            Username :this.state.Username,
+            Password :this.state.Password
+        }
+        axios.post("http://localhost:90/user/login", data)
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
     render(){
         return(
-<form>
-    <p>Username : <input type="text" name="username"/></p>
-    <p>Password : <input type="password" name="password"></input></p>
-   <p><input type="submit"/> </p>
-</form>
+<div>
+    <p>Username : <input type="text" value={this.state.Username}
+     onChange={(event)=>{this.setState({Username : event.target.value})}}/></p>
+    <p>Password : <input type="text" value={this.state.Password} 
+    onChange={(event)=>{this.setState({Password : event.target.value})}}/></p>
+    <p><input type="submit" onClick={this.sendUserData}/>  </p>
+</div>
         )
     }
 }
