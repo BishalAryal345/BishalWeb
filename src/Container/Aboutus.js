ca4fc0fef99e55a7e1f05905ffffd4b5326e05b5
BishalAryal345 <sirajaryal69@gmail.com>
2021-04-09 08:53:53 +0545

    About app

---
+++ src/Container/Aboutus.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import '../CSS/style.css';
+
+class Aboutus extends Component {
+    render() {
+        return (
+            <section id="hero" class="d-flex justify-content-center align-items-center">
+                <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+                    <h1>Learning Today,Leading Tomorrow</h1>
+                        <h2>We are team of talented designers making websites with Bootstrap</h2>
+                        <a href="courses.html" class="btn-get-started">Get Started</a>
+    </div>
+  </section>
+        )
+    }
+}
+export default Aboutus
---
+++ src/Container/RegisterUser.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+
+class registeruser extends Component{
+    render(){
+            return(
+                <div>
+                    This is good
+                </div>
+          
+
+            )
+        }
+    
+
+} 
+export default registeruser
